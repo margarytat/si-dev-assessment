f44689d6d3b9308844ac5aa64b79b38e8715eefb
Margaryta T <margaryta@gmail.com>
2017-07-27 16:39:15 +0000

    task 6 in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,22 @@ class PagesController < ApplicationController
   def sms
     @phone = params[:phone]
     @msg = params[:msg]
+    
+    puts "\n== Preparing to send message =="
+    
+    account_sid = "AC4e6e514986bf159f11205fdb5bd5f7f5" 
+    auth_token = "c27b07542926561dd847e63c64a962ab" 
+    
+    @fromPhone = "+16176185611"
+    @toPhone = "+1"+@phone
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.messages.create(
+        body: @msg,
+        to: @toPhone,    
+        from: @fromPhone)  
+
+    puts message.sid    
 
   end
 end
